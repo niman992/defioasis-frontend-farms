ca0d13271e3cbe9f444875a44bfd399342d7bff3
DeFiOasis <82097091+DeFiOasis@users.noreply.github.com>
2021-04-07 16:06:33 +0200

    Update TwitterCard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'DeFiOasis'
           }}
           options={{
             height: '300',
