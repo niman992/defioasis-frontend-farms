ad20431e9404619543e4fda1394544b85f527b01
niman992 <83501049+niman992@users.noreply.github.com>
2021-05-02 01:57:10 +0200

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,6 +1,6 @@
 export default {
   cake: {
-    56: ''0xFeDb215c9c168456E3E4031F5B4e32DdaeF65f58', // OASIS mainnet to be added
+    56: '0xFeDb215c9c168456E3E4031F5B4e32DdaeF65f58', // OASIS mainnet to be added
     97: '',
   },
   masterChef: {
